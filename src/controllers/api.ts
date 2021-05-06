1ad8033003e5b5a7c008dd81ec9c6e19531fe78f
Mehtaab Gill <mehtaabgill@hotmail.com>
2021-05-05 23:58:22 -0600

    docs(api router): comments for sendAllImageNames

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -2,6 +2,11 @@ import { loadAllImages } from '../helpers/database';
 import { Request, Response } from 'express';
 import { Image } from '../types/images';
 
+/**
+ * API call to retrieve all image names
+ * @param req Request
+ * @param res Response
+ */
 export function sendAllImageNames (req: Request, res: Response) {
     res.json(loadAllImages().map((image: Image) => image.fileName));
 }
