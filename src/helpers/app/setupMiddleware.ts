c05bfb00f68a8dd9e72a395ff8f0d8b3674af6e5
Mehtaab Gill <mehtaabgill@hotmail.com>
2021-05-08 12:21:05 -0600

    feat(app): configured CORS for frontend integration

--- src/helpers/app/setupMiddleware.ts
+++ src/helpers/app/setupMiddleware.ts
@@ -1,10 +1,17 @@
 import { Express } from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import cors from 'cors';
 
 export default function(app: Express) {
     //helmet for basic security
-    app.use(helmet());
+    app.use(helmet({
+        crossOriginResourcePolicy: false
+    }));
+    //cors for allowing resource sharing
+    app.use(cors({
+        origin: '*'
+    }))
     //morgan for logging
     app.use(morgan(':method :url :status - :response-time ms'))
 }
